b83d2416134eb0bb2ae5f590d68fbc729af56a43
cjSound <876304802@qq.com>
2020-08-24 17:39:14 +0800

    膝盖不

--- components/flow-chart/layout.js
+++ components/flow-chart/layout.js
@@ -2,7 +2,7 @@
  * @Author: 曹捷
  * @Date: 2020-08-14 16:32:55
  * @LastEditors: 曹捷
- * @LastEditTime: 2020-08-19 16:46:33
+ * @LastEditTime: 2020-08-20 09:56:30
  * @Description: file content
  */
 
@@ -20,8 +20,8 @@ export default class layout {
         }
         if (this.nodeList && this.nodeList.length > 0) {
             let itemDom = document.getElementById(this.nodeList[0].mainData.uuid)
-            this.nodeWidth = itemDom.offsetWidth
-            this.nodeHeight = itemDom.offsetHeight
+            this.nodeWidth = Math.ceil(itemDom.offsetWidth / 10) * 10
+            this.nodeHeight = Math.ceil(itemDom.offsetHeight / 10) * 10
             // 一行可以放几个元素
             this.rowNum = parseInt((this.maxWidth + this.config.mixX) / (this.nodeWidth + this.config.mixX))
             this.rankLevel()
@@ -40,20 +40,9 @@ export default class layout {
             list.forEach(element => {
                 element.rank = level
                 _this.maxRank = level > _this.maxRank ? level : _this.maxRank
-                let findList = arr.find(item => {
-                    return item.mainData.uuid === element.mainData.uuid
-                })
-                if (!findList || findList.length === 0) {
+                if (!_this._hasExist(arr, element)) {
                     arr.push(element)
-                    if (element.next && element.next.length > 0) {
-                        let nextArr = []
-                        element.next.forEach(element => {
-                            nextArr.push(element.uuid)
-                        });
-                        getRank(nodeList.filter(item => {
-                            return nextArr.indexOf(item.mainData.uuid) !== -1
-                        }), level + 1)
-                    }
+                    getRank(_this.getNextNodes(element), level + 1)
                 }
             });
         }
@@ -63,17 +52,74 @@ export default class layout {
     }
     // 计算每个位置
     judgePostion() {
+        let _this = this
+        let arr = []
         // 大概做几行
         let row = parseInt(this.nodeList.length / this.rowNum)
         let rootList = nodeList.filter(item => {
             return item.prev.length === 0
         })
-
-        function getRank(list, level) {
-
+        let rootY = this.nodeHeight
+        rootList.forEach(element => {
+            element.mainData.pageX = 30
+            element.mainData.pageY = rootY
+            rootY += this.nodeHeight * 3
+            arr.push(element)
+            setChildPos(element)
+        });
+        function setChildPos(parentRoot) {
+            let nextNode = _this.getNextNodes(parentRoot)
+            let nextX = parentRoot.mainData.pageX + _this.config.mixX
+            if (nextNode.length === 1) {
+                nextNode[0].mainData.pageY = parentRoot.mainData.pageY
+                nextNode[0].mainData.pageX = nextX
+                arr.push(nextNode[0])
+            } else if (nextNode.length === 2) {
+                let pageY = parentRoot.pageY - _this.nodeHeight
+                nextNode.forEach(element => {
+                    element.mainData.pageX = nextX
+                    element.mainData.pageY = pageY
+                    pageY = parentRoot.pageY + _this.nodeHeight
+                });
+            } else if (nextNode.length > 2) {
+                let pageY = parentRoot.pageY - _this.nodeHeight
+                nextNode.forEach(element => {
+                    element.mainData.pageX = nextX
+                    element.mainData.pageY = pageY
+                    pageY += _this.nodeHeight * 2
+                })
+            }
         }
     }
-
+    /**
+     * 是否存在当前节点   当前节点是否已经计算过了
+     * @param {*} arr 
+     * 缓存数组
+     * @param {*} element 
+     * 节点对象
+     */
+    _hasExist(arr, element) {
+        let findList = arr.find(item => {
+            return item.mainData.uuid === element.mainData.uuid
+        })
+        return findList && findList.length > 0 ? true : false
+    }
+    /**
+     * 获取  下一级节点 List集合
+     * @param {*} parentRoot 
+     */
+    getNextNodes(parentRoot) {
+        if (parentRoot.next && parentRoot.next.length > 0) {
+            let nextArr = []
+            parentRoot.next.forEach(parentRoot => {
+                nextArr.push(parentRoot.uuid)
+            });
+            return nodeList.filter(item => {
+                return nextArr.indexOf(item.mainData.uuid) !== -1
+            })
+        }
+        return []
+    }
 
 
 }
