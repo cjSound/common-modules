14280d880455eb97bc11ae6855d2520212190383
cjSound <876304802@qq.com>
2020-11-05 17:05:23 +0800

    echarts  新增 支持主题修改

--- utils/echart.js
+++ utils/echart.js
@@ -2,7 +2,7 @@
  * @Author: 曹捷
  * @Date: 2020-10-26 16:36:29
  * @LastEditors: 曹捷
- * @LastEditTime: 2020-11-05 11:19:40
+ * @LastEditTime: 2020-11-05 16:33:14
  * @Description: 图表 原生  Eharts工具类操作
  */
 class echartInfo {
@@ -16,6 +16,9 @@ class echartInfo {
   constructor (vm, options) {
     this.vm = vm
     this.options = options
+    this.typeInfo = {
+      min: '最小值', max: '最大值', average: '平均值', median: '中位数'
+    }
     return this
   }
   /**
@@ -26,8 +29,8 @@ class echartInfo {
    * @param {Object} chartData 
    * chart 默认data ，包含columns顺序和data对应的rows
    * dataset:{
-        dimensions:['key', 'value'],维度
-        source:[] 数据
+        dimensions:['key', 'value'], //维度
+        source:[] //数据
       },
    * @param {Array[String]} listValue 
       和columns  对应的对象key集合['date', 'value']
@@ -42,6 +45,62 @@ class echartInfo {
       arr.push(eleInfo)
     });
     this.options.dataset.source = arr
+  }
+  // 直接设置数据源
+  setData (list) {
+    this.options.dataset.source = list
+  }
+
+
+  /**
+   * 新增图表标线
+   * @param {Array} pointTypes 
+   * this.typeInfo
+   * @param {*} options 
+   * echarts 参数 ，文档参数
+   * https://echarts.apache.org/zh/option.html#series-line.markLine
+   */
+  addMarkPoint (pointTypes, options = {}) {
+    let data = []
+    pointTypes.forEach(element => {
+      data.push({
+        type: element, name: this.typeInfo[element]
+      })
+    });
+    let markPoint = Object.assign({ data }, options)
+    if (Object.prototype.toString.call(this.options.series).indexOf('Object') !== -1) {
+      this.vm.$set(this.options.series, 'markPoint', markPoint)
+    } else if (Object.prototype.toString.call(this.options.series).indexOf('Array') !== -1) {
+      this.options.series.forEach((element, index) => {
+        this.vm.$set(this.options.series[index], 'markPoint', markPoint)
+      });
+    }
+  }
+  /**
+   * 新增图表标线。
+   * @param {*} pointTypes 
+   * this.typeInfo 
+   * @param {*} options 
+   * https://echarts.apache.org/zh/option.html#series-line.markLine
+   */
+  addMarkLine (pointTypes, options = {}) {
+    let data = []
+    pointTypes.forEach(element => {
+      data.push({
+        type: element, name: this.typeInfo[element],
+        label: {
+          formatter: '{b}: {c}'
+        }
+      })
+    });
+    let markLine = Object.assign({ data }, options)
+    if (Object.prototype.toString.call(this.options.series).indexOf('Object') !== -1) {
+      this.vm.$set(this.options.series, 'markLine', markLine)
+    } else if (Object.prototype.toString.call(this.options.series).indexOf('Array') !== -1) {
+      this.options.series.forEach((element, index) => {
+        this.vm.$set(this.options.series[index], 'markLine', markLine)
+      });
+    }
   }
 }
 
