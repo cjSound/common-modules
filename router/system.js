cd7d93718ace0aeaa94e7f6aef9d2dda32bc350a
cjSound <876304802@qq.com>
2020-12-18 09:20:59 +0800

    22

--- router/system.js
+++ router/system.js
@@ -2,38 +2,38 @@
  * @Author: 曹捷
  * @Date: 2020-04-27 14:49:50
  * @LastEditors: 曹捷
- * @LastEditTime: 2020-06-03 13:21:36
+ * @LastEditTime: 2020-12-18 09:02:05
  * @Description: 系统管理路由
  */
-import Layout from '@/common-modules/layout'
-export default [{
-    path: '/system',
-    name: 'systemMain',
-    component: Layout,
-    children: [{
-        path: 'userIndex',
-        name: 'systemUser',
-        component: () => import('@/common-modules/views/system/userManage/userIndex'),
-    },
+import Layout from './../layout/index.vue'
+export default [
     {
-        path: 'roleIndex',
-        name: 'systemRole',
-        component: () => import('@/common-modules/views/system/role/roleIndex'),
-    },
-    {
-        path: 'orgIndex',
-        name: 'systemOrg',
-        component: () => import('@/common-modules/views/system/org/orgIndex')
-    }, {
-        path: 'dicIndex',
-        name: 'systemDcit',
-        component: () => import('@/common-modules/views/system/dict/dicIndex')
-    }, {
-        path: 'menuIndex',
-        name: 'menuMain',
-        component: () => import('@/common-modules/views/system/menu/menuIndex')
-    },
-
-    ]
-}
+        path: '/system',
+        name: 'systemMain',
+        component: Layout,
+        children: [{
+            path: 'userIndex',
+            name: 'systemUser',
+            component: () => import('./../views/system/userManage/userIndex'),
+        },
+        {
+            path: 'roleIndex',
+            name: 'systemRole',
+            component: () => import('./../views/system/role/roleIndex'),
+        },
+        {
+            path: 'orgIndex',
+            name: 'systemOrg',
+            component: () => import('./../views/system/org/orgIndex')
+        }, {
+            path: 'dicIndex',
+            name: 'systemDcit',
+            component: () => import('./../views/system/dict/dicIndex')
+        }, {
+            path: 'menuIndex',
+            name: 'menuMain',
+            component: () => import('./../views/system/menu/menuIndex')
+        },
+        ]
+    }
 ]
