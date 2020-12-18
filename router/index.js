2f81068be88941cf2692f470d16828f5ec8be440
cjSound <876304802@qq.com>
2020-12-18 11:14:39 +0800

    add

--- router/index.js
+++ router/index.js
@@ -2,13 +2,17 @@
  * @Author: 曹捷
  * @Date: 2020-04-22 14:28:38
  * @LastEditors: 曹捷
- * @LastEditTime: 2020-06-02 16:43:14
+ * @LastEditTime: 2020-12-18 11:14:20
  * @Description: 路由基础文件
  */
 import Vue from 'vue'
 import Router from 'vue-router'
 Vue.use(Router)
-import system from './system'
+const modulesFiles = require.context("./templete", true, /\.js$/);
+let modules = modulesFiles.keys().reduce((modules, element) => {
+  modules[element.replace(/^\.\/(\S+)\.js$/, '$1')] = modulesFiles(element).default
+  return modules
+}, {});
 //  name 判断权限的关键字，必须和后台菜单设置里面的menuCode相等，才有此菜单
 export const constantRoutes = [
   {
@@ -32,22 +36,41 @@ export const constantRoutes = [
     redirect: '/404',
     hidden: true
   },
-  ...system
 ]
 
-const createRouter = () => new Router({
-  scrollBehavior: () => ({
-    y: 0
-  }),
-  routes: constantRoutes
-})
+const createRouter = (options = {}) => {
+  let arr = []
+  // 初始化 加载默认模板
+  if (options.template) {
+    if (Object.prototype.toString.call(options.template).indexOf('String') !== -1) {
+      arr.push(options.template)
+    } else if (Object.prototype.toString.call(options.template).indexOf('Array') !== -1) {
+      arr = options.template
+    }
+  }
+  if (arr.length > 0) {
+    arr.forEach(element => {
+      if (modules[element]) {
+        constantRoutes.concat(modules[element])
+      } else {
+        console.error(`obit front platform  找不到 ${element} Router 模块`)
+      }
+    })
+  }
+  return new Router({
+    scrollBehavior: () => ({
+      y: 0
+    }),
+    routes: constantRoutes
+  })
+}
 
-const router = createRouter()
+// const router = createRouter()
 // router.addRoutes(systemRouter)
 
-export function resetRouter() {
+export function resetRouter () {
   const newRouter = createRouter()
   router.matcher = newRouter.matcher // reset router
 }
 
-export default router
+export default createRouter
--- router/templete/system.js
+++ router/templete/system.js
@@ -2,10 +2,10 @@
  * @Author: 曹捷
  * @Date: 2020-04-27 14:49:50
  * @LastEditors: 曹捷
- * @LastEditTime: 2020-12-18 09:02:05
+ * @LastEditTime: 2020-12-18 09:29:39
  * @Description: 系统管理路由
  */
-import Layout from './../layout/index.vue'
+import Layout from './../../layout/index.vue'
 export default [
     {
         path: '/system',
@@ -14,25 +14,25 @@ export default [
         children: [{
             path: 'userIndex',
             name: 'systemUser',
-            component: () => import('./../views/system/userManage/userIndex'),
+            component: () => import('./../../views/system/userManage/userIndex'),
         },
         {
             path: 'roleIndex',
             name: 'systemRole',
-            component: () => import('./../views/system/role/roleIndex'),
+            component: () => import('./../../views/system/role/roleIndex'),
         },
         {
             path: 'orgIndex',
             name: 'systemOrg',
-            component: () => import('./../views/system/org/orgIndex')
+            component: () => import('./../../views/system/org/orgIndex')
         }, {
             path: 'dicIndex',
             name: 'systemDcit',
-            component: () => import('./../views/system/dict/dicIndex')
+            component: () => import('./../../views/system/dict/dicIndex')
         }, {
             path: 'menuIndex',
             name: 'menuMain',
-            component: () => import('./../views/system/menu/menuIndex')
+            component: () => import('./../../views/system/menu/menuIndex')
         },
         ]
     }
